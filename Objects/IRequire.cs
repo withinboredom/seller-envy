670750b177de2b502d2635a54b17583f4454381f
Rob Landers <landers.robert@gmail.com>
2015-12-12 00:09:11 -0500

    And this

---
+++ Objects/IRequire.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace Objects
+{
+    public interface IRequire
+    {
+        Task Initialize(IProcessor processor);
+    }
+}
