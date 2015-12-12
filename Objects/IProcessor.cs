da19320eedcff106ff18f33b9f2f22360dd8b346
Rob Landers <landers.robert@gmail.com>
2015-12-12 00:08:59 -0500

    Yeah, need this

---
+++ Objects/IProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Objects
+{
+    public interface IProcessor
+    {
+        Task<bool> SendCommand<TAggregate, TCommand>(TCommand command)
+            where TAggregate : Aggregate, new()
+            where TCommand : ICommandEvent;
+
+        Task<TAggregate> GetAggregate<TAggregate>(Guid id)
+            where TAggregate : Aggregate, new();
+    }
+}
